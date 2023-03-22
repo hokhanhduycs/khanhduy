bf1b42c94e4c798af10d5ee3676ce1d5391ecd70
hokhanhduycs <hokhanhduycs@gmail.com>
2023-03-22 18:17:18 +0700

    add game flappy bird

--- src/router/index.js
+++ src/router/index.js
@@ -5,10 +5,11 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
+      path: '/home',
       name: 'home',
       component: HomeView
     },
+
     {
       path: '/about',
       name: 'about',
@@ -16,7 +17,18 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+
+    {
+      path: '/',
+      name: 'test',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('@/game/Flappy-Bird/FlappyBird.vue')
     }
+
+
   ]
 })
 
